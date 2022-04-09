bdead27a2a5a3c640dd6d4ab7563af222ac6068c
Rl242Dev <74558778+Rl242Dev@users.noreply.github.com>
2022-04-09 11:46:24 +0200

    Update data.py

--- data.py
+++ data.py
@@ -2,28 +2,18 @@ import mysql.connector
 
 db = mysql.connector.connect(
     host='localhost',
-    database='ArmaPanel',
-    user='rl242',
-    password='31#Nigi2'
+    database='ArmaPanel', # Your database name
+    user='****', # Your username in mysql
+    password='*****' # Your password in mysql
 )
 
 mycursor = db.cursor()
 
-sql = "INSERT INTO ArmaPanel.ArmaData (name, gang, bank, weapon) VALUES (%s, %s, %s, %s)"
+sql = "INSERT INTO db_name.table_name (column_1, column_2, column_3, column_4) VALUES (%s, %s, %s, %s)"
 val = [
-    ('Ramuel Eag', 'Mercenaires', 7.2, 'Zafir 7.62'),
-    ('Antonio Zook', 'Mercenaires', 14.7, 'Zafir 7.62'),
-    ('Jack Andersonn', 'RLT', 2.1, 'Mk1 7.62'),
-    ('Tac Campbell', 'Police', 4.4, 'Lynx 12.7'),
-    ('Pierre pierron', 'BouBoule Gang', 17.4, 'Zafir 7.62'),
-    ('Calvin M', 'Police', 8.9, 'Lynx 12.7'),
-    ('Rachid L', 'RLT', 1.7, 'Zafir 7.62'),
-    ('Raphael Nerleur', 'Medecins', 5.1, 'Null'),
-    ('Nass Paco', 'CFRG', 2.8, 'Mk1 7.62'),
-    ('Ali Boulawa', 'RLT', 1.8, 'Mk1 7.62'),
-    ('Julien Gambini', 'Police', 11.2, 'Zafir 7.62'),
-    ('Null', 'Null', 0.0, 'Null')
+    ('***', '***', ***, '***'), # char, char, int/float, char
+    ('***', '***', ***, '***'), # char, char, int/float, char
 ]
 
 mycursor.executemany(sql, val)
-db.commit()
+db.commit() 
